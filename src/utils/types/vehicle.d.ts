dc69776a211d16bfa64da8d0e59265b3a5752c67
Alexander Aguirre <keevinaguirre@gmail.com>
2024-03-07 15:20:45 +0100

    Refactor VehicleService to use RootObject type

--- src/utils/types/vehicle.d.ts
+++ src/utils/types/vehicle.d.ts
@@ -48,3 +48,114 @@ export type Vehicle = {
 	status?: string
 	imageUrl?: string
 }
+
+export type RootObject = {
+	data: Data
+	meta: Meta
+}
+
+export type Data = {
+	agility?: Agility
+	cargo_capacity?: number
+	chassis_id?: number
+	crew?: Crew
+	description?: Description
+	foci?: Description[]
+	fuel?: Fuel
+	health?: number
+	id?: number
+	insurance?: Insurance
+	manufacturer: Manufacturer
+	mass?: any
+	missing_translations?: any[]
+	msrp?: number
+	name?: string
+	personal_inventory_capacity?: number
+	production_note?: Description
+	production_status?: Description
+	quantum?: Quantum
+	size?: Description
+	sizes?: Sizes
+	slug?: string
+	speed?: Speed
+	type?: Description
+	updated_at?: Date
+	uuid?: any
+}
+
+export type Agility = {
+	acceleration?: Acceleration
+	pitch?: any
+	roll?: any
+	yaw?: any
+}
+
+export type Acceleration = {
+	main?: number
+	retro?: number
+	vtol?: number
+}
+
+export type Crew = {
+	max?: number
+	min?: number
+	operation?: number
+	weapon?: number
+}
+
+export type Description = {
+	de_DE?: string
+	en_EN?: string
+}
+
+export type Fuel = {
+	capacity?: number
+	intake_rate?: number
+	usage?: Usage
+}
+
+export type Usage = {
+	main?: number
+	maneuvering?: number
+	retro?: number
+	vtol?: number
+}
+
+export type Insurance = {
+	claim_time?: number
+	expedite_cost?: number
+	expedite_time?: number
+}
+
+export type Manufacturer = {
+	code: string
+	name: string
+}
+
+export type Quantum = {
+	quantum_fuel_capacity?: number
+	quantum_range?: number
+	quantum_speed?: number
+	quantum_spool_time?: number
+}
+
+export type Sizes = {
+	beam?: number
+	height?: number
+	length?: number
+}
+
+export type Speed = {
+	afterburner?: any
+	max?: number
+	max_to_zero?: number
+	scm?: any
+	scm_to_zero?: number
+	zero_to_max?: number
+	zero_to_scm?: number
+}
+
+export type Meta = {
+	processed_at?: Date
+	valid_relations?: string[]
+}
